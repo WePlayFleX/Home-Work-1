79a1c1e55e9b0a0c47f0113480bb0e7783400287
WePlayFleX <alex.kamai@ya.ru>
2021-11-28 22:47:30 +0300

    Добавьте файлы проекта.

---
+++ Home Work 1/HW1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " \"To be, or not to be: that is the question: \n";
+	cout << " Whether \'tis nobler in the mind to suffer \n";
+	cout << " The slings and arrows of outrageous fortune, \n";
+	cout << " Or to take arms against a sea of troubles,\n";
+	cout << " And by opposing end them\?\"\n";
+	cout << " \t \t \t William Shakespeare \n";
+	system("pause");
+	return 0;
+}
